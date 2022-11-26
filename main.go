02347c4b0150051412b0afde718e7fb57d3eaf18
Santiago Passafiume <santiagopassafiume@Santiagos-MacBook-Air.local>
2022-11-26 18:06:16 -0300

    implement logic

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Printf("")
+	reader := bufio.NewReader(os.Stdin)
+
+	firstNumber, _ := getInput("Enter first number: ", reader)
+	secondNumber, _ := getInput("Enter second number: ", reader)
+	parsedFirstNumber, _ := strconv.ParseFloat(firstNumber, 64)
+	parsedSecondNumber, _ := strconv.ParseFloat(secondNumber, 64)
+
+	option, _ := getInput("a (addition) - s (substraction) - d (division) - m (multiplication): ", reader)
+
+	chooseOption(option, parsedFirstNumber, parsedSecondNumber)
 }
